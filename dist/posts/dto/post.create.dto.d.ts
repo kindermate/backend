62841b6f545f1f4901ae2f5d683793a135a0dc85
buzzrstudio <keepsoul@gmail.com>
2022-06-27 06:47:50 +0900

    add: 미션 UI 업데이트

--- dist/posts/dto/post.create.dto.d.ts
+++ dist/posts/dto/post.create.dto.d.ts
@@ -1,5 +1,5 @@
 import { Post } from '@/posts/schema/post.schema';
-declare const PostCreateDto_base: import("@nestjs/common").Type<Pick<Post, "title" | "content" | "thumbnail" | "startDate" | "endDate" | "isPublic" | "isNotice">>;
+declare const PostCreateDto_base: import("@nestjs/common").Type<Pick<Post, "startDate" | "title" | "content" | "thumbnail" | "endDate" | "isPublic" | "isNotice">>;
 export declare class PostCreateDto extends PostCreateDto_base {
 }
 export {};
